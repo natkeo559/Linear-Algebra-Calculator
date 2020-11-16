6f5117e7e815d8ea85b1c0942e286744046b05aa
Nathan <natkeo1@gmail.com>
2020-11-16 13:46:19 -0500

    Eigenvalues and Eigenvectors Added

---
+++ Eig.py
@@ -0,0 +1,9 @@
+from numpy.linalg import eig
+
+def eigenvals(mtx):
+    e = linalg.eig(mtx)[0]
+    return e
+
+def eigenvects(mtx):
+    v = [linalg.eig(mtx)[1][:,i] for i in range(len(linalg.eig(mtx)[1]))]
+    return v
