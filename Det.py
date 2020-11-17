457ecb819ba00ede495dafc93380cff0b9ae40b3
Nathan <natkeo1@gmail.com>
2020-11-16 23:37:48 -0500

    Updated Determinant

--- Det.py
+++ Det.py
@@ -4,11 +4,10 @@ def det(mtx):
     d = 0
     x = array(mtx)
     if len(mtx[0]) == 2:
-        return (x[0][0]*x[1][1])-(x[0][1]*x[1][0])
+        return (x[0][0] * x[1][1] - x[0][1] * x[1][0])
     else:
         for i in range(len(mtx[0])):
             x = delete(mtx,0,0)
             x = delete(x,i,1)
-            d += ((((-1)**i)*(mtx[0][i]))*det(x))
+            d += ((-1) ** i) * mtx[0][i] * det(x)
     return d
-    
